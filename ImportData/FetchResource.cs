67a45f237b499f4bd9dcff324daa4c7c503f93fa
AgnieszkaJania <65187859+AgnieszkaJania@users.noreply.github.com>
2024-02-06 13:59:15 +0100

    added class to get data from API and deserialize JSON data

---
+++ ImportData/FetchResource.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ProcessData
+{
+    public static class FetchResource
+    {
+        public static async Task<string> FetchResourceAsync(string address)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync(address);
+                    var data = await response.Content.ReadAsStringAsync();
+                    return data;
+                }
+            }
+            catch(HttpRequestException ex)
+            {
+                return ex.Message;
+            }
+        }
+        public static List<Rate> DeserializeRates(string json)
+        {
+            var rates = JsonConvert.DeserializeObject<List<ExchangeRate>>(json);
+            return rates.First().Rates;
+        }  
+    }
+}
