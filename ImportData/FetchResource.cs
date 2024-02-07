0ab93903a48921569fc40b1891470c88ae1b60f3
AgnieszkaJania <65187859+AgnieszkaJania@users.noreply.github.com>
2024-02-07 15:43:30 +0100

    added method to deserialize rates from xml

--- ImportData/FetchResource.cs
+++ ImportData/FetchResource.cs
@@ -1,8 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ProcessData
 {
@@ -28,6 +30,19 @@ namespace ProcessData
         {
             var rates = JsonConvert.DeserializeObject<List<ExchangeRate>>(json);
             return rates.First().Rates;
-        }  
+        }
+        public static List<Rate> DeserializeRatesFromXml(string xml)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            List<Rate> rates = new List<Rate>();
+            xmlDoc.LoadXml(xml);
+            foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes[0].ChildNodes[3].ChildNodes)
+            {
+                rates.Add(new Rate { Currency = node.ChildNodes[0].InnerText, Code = node.ChildNodes[1].InnerText,
+                     Mid = decimal.Parse(node.ChildNodes[2].InnerText, NumberFormatInfo.InvariantInfo)
+                });
+            }
+            return rates;
+        }
     }
 }
